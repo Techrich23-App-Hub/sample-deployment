b139e2fae443c15ac82f8bd14ef6c63249c2a9b5
Techrich23-App-Hub <122430721+Techrich23-App-Hub@users.noreply.github.com>
2023-02-21 12:13:55 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <! Doctype html>  
 <html lang="en">  
-<hea
+
   <meta charset="utf-8">  
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
   <title> PHP Registration Form Example </title>  
